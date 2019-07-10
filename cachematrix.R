436329ae0a0fdb920bfb6b0e8d1211264ce15398
Ivan Makukhin <ivan.makukhin@gmail.com>
2019-07-10 19:27:51 +0300

    UPD

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## list with all options in
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -9,6 +7,7 @@ makeCacheMatrix <- function(x = matrix()) {
   set <- function(y){
     x<<- y
     m<<- NULL
+    
   }
   
   get <- function () x
